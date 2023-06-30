f5fb296a0553dc24cedc957fff11c863e6f4372f
Responsibleuk <james@we-are-responsible.com>
2023-06-30 17:19:20 +0000

    Add

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,49 +1,92 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var portfolioSection = document.getElementById('portfolio');
-    var image = portfolioSection.querySelector('.color-splash img');
-    var canvas = portfolioSection.querySelector('#color-splash-canvas');
+    var canvas = document.getElementById('constellation-canvas');
     var context = canvas.getContext('2d');
-    var observerOptions = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5 // Adjust the threshold as needed
-    };
+    var stars = [];
+    var numStars = 100; // Adjust the number of stars as needed
   
-    var observer = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          applyColorSplash();
-          observer.unobserve(entry.target);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  
+    createStars();
+    animate();
+  
+    function createStars() {
+      for (var i = 0; i < numStars; i++) {
+        var x = Math.random() * canvas.width;
+        var y = Math.random() * canvas.height;
+        var dx = (Math.random() - 0.5) * 2; // Random velocity in x direction
+        var dy = (Math.random() - 0.5) * 2; // Random velocity in y direction
+        stars.push({ x: x, y: y, dx: dx, dy: dy });
+      }
+    }
+  
+    function animate() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+  
+      for (var i = 0; i < stars.length; i++) {
+        updateStarPosition(stars[i]);
+        drawStar(stars[i].x, stars[i].y);
+      }
+  
+      connectStars();
+  
+      requestAnimationFrame(animate);
+    }
+  
+    function updateStarPosition(star) {
+      star.x += star.dx;
+      star.y += star.dy;
+  
+      // Wrap the stars around the canvas edges
+      if (star.x < 0) {
+        star.x = canvas.width;
+      } else if (star.x > canvas.width) {
+        star.x = 0;
+      }
+      if (star.y < 0) {
+        star.y = canvas.height;
+      } else if (star.y > canvas.height) {
+        star.y = 0;
+      }
+    }
+  
+    function drawStar(x, y) {
+      context.beginPath();
+      context.arc(x, y, 2, 0, Math.PI * 2, false);
+      context.fillStyle = '#000000'; // Set the fill color to black
+      context.fill();
+      context.closePath();
+    }
+  
+    function connectStars() {
+      for (var i = 0; i < stars.length; i++) {
+        for (var j = i + 1; j < stars.length; j++) {
+          var dx = stars[i].x - stars[j].x;
+          var dy = stars[i].y - stars[j].y;
+          var distance = Math.sqrt(dx * dx + dy * dy);
+  
+          if (distance < 150) { // Adjust the distance threshold as needed
+            context.beginPath();
+            context.moveTo(stars[i].x, stars[i].y);
+            context.lineTo(stars[j].x, stars[j].y);
+            context.strokeStyle = '#000000'; // Set the stroke color to black
+            context.lineWidth = 0.5; // Adjust the line width as needed
+            context.stroke();
+            context.closePath();
+          }
         }
-      });
-    }, observerOptions);
-  
-    observer.observe(portfolioSection);
-  
-    function applyColorSplash() {
-      image.onload = function() {
-        canvas.width = image.width;
-        canvas.height = image.height;
-  
-        context.drawImage(image, 0, 0);
-        grayscale();
-      };
-  
-      function grayscale() {
-        var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        var data = imageData.data;
-  
-        for (var i = 0; i < data.length; i += 4) {
-          var grayscale = data[i] * 0.3 + data[i + 1] * 0.59 + data[i + 2] * 0.11;
-          data[i] = grayscale; // Red
-          data[i + 1] = grayscale; // Green
-          data[i + 2] = grayscale; // Blue
-        }
-  
-        context.putImageData(imageData, 0, 0);
       }
     }
   });
-
   
+  
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var curtainContainer = document.querySelector('.curtain-container');
+    var curtain = document.querySelector('.curtain');
+  
+    curtainContainer.addEventListener('click', function() {
+      curtain.classList.toggle('curtain-open');
+    });
+  });
   
