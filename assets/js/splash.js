90f64cb3cdf9cc948994aef1ebb1a6f05f508799
Responsibleuk <james@we-are-responsible.com>
2023-07-01 14:53:48 +0000

    Add

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,29 +1,92 @@
- // Get the canvas element
- const canvas = document.getElementById('output-canvas');
- const context = canvas.getContext('2d');
+window.addEventListener("DOMContentLoaded", function() {
+  combineImages();
+});
 
- // Load the images
- const image1 = new Image();
- const image2 = new Image();
+function combineImages() {
+  // Create a canvas element
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
 
- image1.src = '../assets/images/laptop.png';
- image2.src = '../assets/images/colour.png';
+  // Load the images
+  var laptopImage = new Image();
+  laptopImage.src = "../assets/images/laptop.png";
 
- // Wait for the images to load
- Promise.all([loadImage(image1), loadImage(image2)])
-   .then(() => {
-     // Set the canvas size to match the image size
-     canvas.width = image1.width;
-     canvas.height = image1.height;
+  var colorImage = new Image();
+  colorImage.src = "../assets/images/colour.png";
 
-     // Draw the images onto the canvas
-     context.drawImage(image1, 0, 0);
-     context.drawImage(image2, 0, 0);
-   });
+  var storyxImage = new Image();
+  storyxImage.src = "../assets/images/storyx.png";
 
- function loadImage(image) {
-   return new Promise((resolve, reject) => {
-     image.onload = resolve;
-     image.onerror = reject;
-   });
- }
+  // Wait for the images to load
+  Promise.all([
+    new Promise((resolve, reject) => {
+      laptopImage.onload = resolve;
+      laptopImage.onerror = reject;
+    }),
+    new Promise((resolve, reject) => {
+      colorImage.onload = resolve;
+      colorImage.onerror = reject;
+    }),
+    new Promise((resolve, reject) => {
+      storyxImage.onload = resolve;
+      storyxImage.onerror = reject;
+    })
+  ])
+    .then(() => {
+      // Set the canvas size to accommodate all the images
+      canvas.width = laptopImage.width * 2 + colorImage.width;
+      canvas.height = Math.max(laptopImage.height, colorImage.height, storyxImage.height);
+
+      // Calculate the positions for the images
+      var laptopX = canvas.width / 2 - laptopImage.width / 2;
+      var laptopY = canvas.height / 2 - laptopImage.height / 2;
+      var colorX = laptopX + laptopImage.width;
+      var storyxX = laptopX - storyxImage.width;
+
+      // Animate the images
+      var slideDistanceColor = colorX - laptopX;
+      var slideSpeedColor = slideDistanceColor / 500; // Adjust the slide speed as needed
+
+      var slideDistanceStoryx = laptopX - storyxX;
+      var slideSpeedStoryx = slideDistanceStoryx / 500; // Adjust the slide speed as needed
+
+      var slideProgressColor = 0;
+      var slideProgressStoryx = 0;
+
+      function animate() {
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw the laptop image
+        ctx.drawImage(laptopImage, laptopX, laptopY);
+
+        // Calculate the current positions of the color and storyx images
+        var currentColorX = colorX - slideProgressColor;
+        var currentStoryxX = storyxX + slideProgressStoryx;
+
+        // Draw the color image at the updated position
+        ctx.drawImage(colorImage, currentColorX, laptopY);
+
+        // Draw the storyx image at the updated position
+        ctx.drawImage(storyxImage, currentStoryxX, laptopY);
+
+        // Update the slide progress
+        slideProgressColor += slideSpeedColor;
+        slideProgressStoryx += slideSpeedStoryx;
+
+        // Continue the animation until both images reach their target positions
+        if (slideProgressColor <= slideDistanceColor || slideProgressStoryx <= slideDistanceStoryx) {
+          requestAnimationFrame(animate);
+        }
+      }
+
+      // Start the animation after a delay
+      setTimeout(animate, 1000);
+    })
+    .catch(handleError);
+}
+
+function handleError(error) {
+  console.error("Error loading images:", error);
+}
+
