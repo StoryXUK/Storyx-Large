38556030b8422c10d275d058ec06040ab63b7461
Responsibleuk <james@we-are-responsible.com>
2023-07-09 13:02:04 +0000

    Add

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,11 +1,4 @@
 window.addEventListener("load", function() {
     var preloader = document.getElementById("preloader");
-    var content = document.getElementById("content");
-    
-    setTimeout(function() {
-      preloader.classList.add("hidden");
-      content.classList.add("show");
-      content.style.left = "0";
-      document.body.style.overflow = "auto";
-    }, 3000); // Adjust the delay as needed
+    preloader.style.display = "none";
   });
